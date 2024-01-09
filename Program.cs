e8ddd6cfbaf87f46e61e7bde2df7a396ac78ec11
Felipe Brandao <brandao154@outlook.com>
2024-01-09 13:46:30 -0300

    first version

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ExercicioEnum.Entities;
+using ExercicioEnum.Entities.Enums;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Order order = new Order
+        {
+            Id = 1080,
+            Moment = DateTime.Now,
+            Status = OrderStatus.PendingPayment
+        };
+
+        Console.WriteLine(order);
+        string txt = OrderStatus.PendingPayment.ToString();
+        OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+        Console.WriteLine(os);
+        Console.WriteLine(txt);
+    }
+}
